2a1c394e542c894a0552bf1e3e483dad5a93fe84
geraenrique97 <geraenrique97@hotmail.com>
2019-04-29 03:48:29 -0300

    2.1

--- src/app/services/registrar.service.ts
+++ src/app/services/registrar.service.ts
@@ -7,15 +7,16 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 export class RegistrarService {
 
   constructor(private http:HttpClient) { }
-  addUser(body: object){
+  addUser(body2: object){
     const headers=new HttpHeaders({
       'X-Requested/with':'XMLHttpRequest',
       'Content-Type':'aplication/json'
 
     });
-    // const url:string=""
-    // return this.http.post(url,body,{httpHeaders})
-    // http://localhost:8000/api/auth/signup
+    let body:{
+      //aca va el json con todos los campos que pones en el postman
+    }
+    
     const url:string=`http://localhost:8000/api/auth/signup`;
     return this.http.post(url, body, {headers})
 
